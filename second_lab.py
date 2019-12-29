a18a65d945a6dd84fb27323b462bc28596c4fac8
antipups <nikitakurkurin@protonmail.ch>
2019-12-29 22:27:15 +0300

    2-ая лаба 20ый вариант

--- second_lab.py
+++ second_lab.py
@@ -25,33 +25,10 @@ class MyApp(App):
                 text_input_for_add_main_ls.text = ''
                 text_input_for_add_main_ls.hint_text_color = [1, 0, 0, 1]
 
-        def add_in_second_list(instance):   # функция на добавление во второстепенный список, тоже самое что и в мейн только список другой
-            if text_input_for_add_sec_ls.text:
-                self.second_list.append(text_input_for_add_sec_ls.text)
-                text_input_for_add_sec_ls.hint_text = 'Элемент - ' + text_input_for_add_sec_ls.text + ' добавлен.'
-                text_input_for_add_sec_ls.text = ''
-                text_input_for_add_sec_ls.hint_text_color = [0, .7, 0, 1]
-                text_input_second_ls.text = ' -> '.join(self.second_list)
-            else:
-                text_input_for_add_sec_ls.hint_text = 'Ошибка добавления элемента'
-                text_input_for_add_sec_ls.text = ''
-                text_input_for_add_sec_ls.hint_text_color = [1, 0, 0, 1]
-
         def task(instance):
-            try:
-                need_elem = int(text_input_for_task.text) - 1   # так как перед, то делаем ещё - 1
-            except ValueError:
-                text_input_for_task.hint_text_color = [1, 0, 0, 1]
-                text_input_for_task.text = ''
-                text_input_for_task.hint_text = 'Введите число!!!'
-            else:
-                if len(self.main_list) <= need_elem or need_elem < 0:   # если введенный пользователем индекс - хуйня
-                    text_input_for_task.hint_text_color = [1, 0, 0, 1]
-                    text_input_for_task.text = ''
-                    text_input_for_task.hint_text = 'Индекс неверный'
-                else:
-                    self.main_list.insert(need_elem, ' -> '.join(self.second_list))
-                    text_input_main_ls.text = ' -> '.join(self.main_list)
+            if len(self.main_list) > 1:
+                self.main_list = [self.main_list[-1]] + self.main_list[1:-1] + [self.main_list[0]]
+                text_input_main_ls.text = ' -> '.join(self.main_list)
 
         main = GridLayout(cols=2,  # главный лайаут. в нем все и будет
                           )
@@ -63,26 +40,12 @@ class MyApp(App):
         text_input_main_ls = TextInput(hint_text='Главный список',
                                        readonly=True,
                                        )
-        text_input_for_add_sec_ls = TextInput(hint_text='Введите элемент для\nвторостепенного списка списка',
-                                              multiline=False,
-                                              on_text_validate=add_in_second_list   # привязываем к поле ввода функцию - её действия (при энтер)
-                                              )
-        button_for_second_ls = Button(text='Добавить элемент во\nвторостепенный список список',
-                                      on_press=add_in_second_list)  # привязываем к кнопке функцию - её действия
-        text_input_second_ls = TextInput(hint_text='Второстепенный список',
-                                         readonly=True)
         # далее, тупо добавляем всё в главное окно
         main.add_widget(text_input_for_add_main_ls)
         main.add_widget(button_for_main_ls)
-        main.add_widget(text_input_for_add_sec_ls)
-        main.add_widget(button_for_second_ls)
         main.add_widget(text_input_main_ls)
-        main.add_widget(text_input_second_ls)
-        text_input_for_task = TextInput(hint_text='Введите перед каким элементом\nв главном списке вставить\n'
-                                                  'второстепенный список')
         button_for_task = Button(text='Пуск',
                                  on_press=task)   # привязываем к кнопке функцию - её действия
-        main.add_widget(text_input_for_task)
         main.add_widget(button_for_task)
         return main
 
